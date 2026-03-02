088c23c0f14b07dc556c89736162df2d5ef4c3cd
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-01 20:34:49 -0800

    Added APP

--- nix/shell.nix
+++ nix/shell.nix
@@ -3,6 +3,10 @@ let
   py = pkgs.python313Packages;
 in {
   packages.default = py.qualifiers-mcp;
+  apps.default = {
+    type = "app";
+    program = "${py.qualifiers-mcp}/bin/serve-mcp";
+  };
   devShells.default = pkgs.mkShell {
     packages = with py; [
       qualifiers-mcp
